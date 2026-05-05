5cd20c239c6e6ac08e46f61f3e56b49e9b82a77d
Rangga Adhitya <deneuv3.4@gmail.com>
2026-05-05 08:21:47 +0700

    redesign: experience section as vertical timeline with cards
    - Vertical line with circular nodes (Briefcase icon)
    - Cards alternate left/right on desktop
    - Mobile: all cards on right of timeline
    - Period shown in accent-warm color
    - Hover: node glows gold, card shifts slightly
    - All details visible (no hover-to-reveal)
    - Staggered scroll-in animation

--- src/components/ExperienceSection.vue
+++ src/components/ExperienceSection.vue
@@ -2,11 +2,12 @@
 import { onMounted } from 'vue'
 import { profile } from '../data/profile'
 import { useScrollAnimation } from '../composables/useScrollAnimation'
+import { Briefcase } from 'lucide-vue-next'
 
 const { animateOnScroll } = useScrollAnimation()
 
 onMounted(() => {
-  animateOnScroll('#experience .exp-animate', { y: 20, duration: 0.5, stagger: 0.08 })
+  animateOnScroll('#experience .exp-animate', { y: 30, duration: 0.6, stagger: 0.12 })
 })
 </script>
 
@@ -18,19 +19,23 @@ onMounted(() => {
         <h2 class="section-heading">Where I've worked</h2>
       </div>
 
-      <div class="experience__list">
-        <div
-          v-for="exp in profile.experience"
-          :key="exp.company"
-          class="experience__item card card--interactive exp-animate"
-        >
-          <div>
-            <div class="experience__company">{{ exp.company }}</div>
-          </div>
-          <div class="experience__role">{{ exp.role }}</div>
-          <div class="experience__period">{{ exp.period }}</div>
+      <div class="experience__timeline">
+        <div class="experience__line" />
 
-          <div class="experience__detail">
+        <div
+          v-for="(exp, index) in profile.experience"
+          :key="exp.company"
+          class="experience__item exp-animate"
+          :class="{ 'experience__item--right': index % 2 !== 0 }"
+        >
+          <div class="experience__node">
+            <Briefcase :size="12" />
+          </div>
+
+          <div class="experience__card">
+            <div class="experience__period">{{ exp.period }}</div>
+            <h3 class="experience__company">{{ exp.company }}</h3>
+            <p class="experience__role">{{ exp.role }}</p>
             <p class="experience__description">{{ exp.description }}</p>
             <div class="experience__highlights">
               <span
