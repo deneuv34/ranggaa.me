0fa25978e84d297cba722aa9d9cc91cbab2b77be
Rangga Adhitya <deneuv3.4@gmail.com>
2026-05-05 08:13:40 +0700

    fix: static centered logos instead of marquee (only 3 logos)

--- src/components/CompaniesSection.vue
+++ src/components/CompaniesSection.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
 import { profile } from '../data/profile'
-
-const marqueeLogos = [...profile.featuredCompanies, ...profile.featuredCompanies, ...profile.featuredCompanies, ...profile.featuredCompanies]
 </script>
 
 <template>
@@ -9,19 +7,14 @@ const marqueeLogos = [...profile.featuredCompanies, ...profile.featuredCompanies
     <div class="companies__inner">
       <p class="section-label companies__label">Trusted by</p>
 
-      <div class="companies__track-wrapper">
-        <div class="companies__fade-left" />
-        <div class="companies__fade-right" />
-
-        <div class="companies__track">
-          <img
-            v-for="(company, i) in marqueeLogos"
-            :key="`${company.name}-${i}`"
-            :src="company.logo"
-            :alt="company.name"
-            class="companies__logo"
-          />
-        </div>
+      <div class="companies__logos">
+        <img
+          v-for="company in profile.featuredCompanies"
+          :key="company.name"
+          :src="company.logo"
+          :alt="company.name"
+          class="companies__logo"
+        />
       </div>
     </div>
   </section>
